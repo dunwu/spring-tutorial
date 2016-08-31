6ad378f76c9311a0cdcfeb98e183bd415fd99f46
atlantis1024 <atlantis1024@163.com>
2016-08-31 15:51:17 +0800

    将spring-scheduler中log4j升级为logback；增加实时调度范例

---
+++ spring-scheduler/src/main/java/org/zp/notes/spring/scheduler/DemoTask.java
@@ -0,0 +1,18 @@
+package org.zp.notes.spring.scheduler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @description 一个调度任务例子
+ * @author zhangpeng0913
+ * @date 2016年8月31日
+ */
+public class DemoTask implements Runnable {
+    final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public void run() {
+        logger.info("call DemoTask.run");
+    }
+}
