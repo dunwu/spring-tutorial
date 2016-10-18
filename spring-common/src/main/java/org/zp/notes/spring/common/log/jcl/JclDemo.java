fd91572388d15fe28d861f67e3b84e0435c0e287
atlantis1024 <atlantis1024@163.com>
2016-10-18 20:29:09 +0800

    修改pom.xml

--- spring-common/src/main/java/org/zp/notes/spring/common/log/jcl/JclDemo.java
+++ spring-common/src/main/java/org/zp/notes/spring/common/log/jcl/JclDemo.java
@@ -4,16 +4,16 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 public class JclDemo {
-//    private static final Log log = LogFactory.getLog(JclTest.class);
+    private static final Log log = LogFactory.getLog(JclDemo.class);
 
     public static void main(String[] args) {
-//        String msg = "print log, current level: ";
-//        log.trace(msg + "trace");
-//        log.debug(msg + "debug");
-//        log.info(msg + "info");
-//        log.warn(msg + "warn");
-//        log.error(msg + "error");
-//        log.fatal(msg + "fatal");
+        String msg = "print log, current level: ";
+        log.trace(msg + "trace");
+        log.debug(msg + "debug");
+        log.info(msg + "info");
+        log.warn(msg + "warn");
+        log.error(msg + "error");
+        log.fatal(msg + "fatal");
 
         test();
     }
