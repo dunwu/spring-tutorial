464b308159165daf869f8f4e43ffe5d49cec2bd7
victor zhang <atlantis1024@163.com>
2017-04-14 10:39:24 +0800

    spring-jdbc项目中包含spring-data内容，更名为更合理的名称：spring-db

---
+++ spring-db/README.md
@@ -0,0 +1,22 @@
+# spring-db
+
+> 本项目展示多种数据库与spring-jdbc和spring-data整合的示例。
+
+## spring-jdbc
+
+> 本项目当前展示了对于H2/Mysql的JDBC操作。
+
+### 使用说明
+
+- `org.zp.notes.spring.jdbc.H2JdbcTest` 测试了H2的JDBC操作。
+
+- `org.zp.notes.spring.jdbc.MysqlJdbcTest` 测试了Mysql的JDBC操作。
+
+- `org.zp.notes.spring.jdbc.JdbcDemo` 展示了Java方式使用JDBC（非注解方式）。
+
+
+## spring-data
+
+`org.zp.notes.spring.data.mongo` 包展示了MongoDB的使用。
+
+`org.zp.notes.spring.data.redis.RedisTest` 展示了Redis的使用。
---
+++ spring-db/src/main/java/org/zp/notes/spring/data/redis/IUserDao.java
@@ -0,0 +1,41 @@
+package org.zp.notes.spring.data.redis;
+
+import org.zp.notes.spring.data.redis.bean.UserDTO;
+
+import java.util.List;
+
+/**
+ * @author victor zhang
+ * @date 2017/4/12.
+ */
+public interface IUserDao {
+    /**
+     * 新增
+     */
+    boolean add(UserDTO UserDTO);
+
+    /**
+     * 批量新增 使用pipeline方式
+     */
+    boolean add(List<UserDTO> list);
+
+    /**
+     * 删除
+     */
+    void delete(String key);
+
+    /**
+     * 删除多个
+     */
+    void delete(List<String> keys);
+
+    /**
+     * 修改
+     */
+    boolean update(UserDTO userDTO);
+
+    /**
+     * 通过key获取
+     */
+    UserDTO get(String keyId);
+}
