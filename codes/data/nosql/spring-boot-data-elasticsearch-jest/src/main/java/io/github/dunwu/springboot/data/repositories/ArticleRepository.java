2a38984d5f290caaebe357b2d42e7a68f9cab2a7
dunwu <forbreak@163.com>
2022-10-08 08:57:30 +0800

    feat: 删除 spring-boot-data-elasticsearch-jest

--- codes/data/nosql/spring-boot-data-elasticsearch-jest/src/main/java/io/github/dunwu/springboot/data/repositories/ArticleRepository.java
+++
@@ -1,7 +0,0 @@
-package io.github.dunwu.springboot.data.repositories;
-
-import io.github.dunwu.springboot.data.entities.Article;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface ArticleRepository extends ElasticsearchRepository<Article, String> {
-}
