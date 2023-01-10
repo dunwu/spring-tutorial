745a6f6777da4535ecb9d6e9d848ea44112caed5
dunwu <forbreak@163.com>
2023-01-10 15:12:09 +0800

    feat: Spring 校验、转换示例更新

---
+++ codes/core/spring-validation/src/main/java/io/github/dunwu/spring/core/ValidationApplication.java
@@ -0,0 +1,26 @@
+package io.github.dunwu.spring.core;
+
+import io.github.dunwu.spring.core.validation.config.CustomValidatorConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * 启动类
+ *
+ * @author <a href="mailto:forbreak@163.com">Zhang Peng</a>
+ * @date 2022-12-26
+ */
+@SpringBootApplication
+public class ValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValidationApplication.class, args);
+    }
+
+    @Bean
+    public CustomValidatorConfig personValidatorConfig() {
+        return new CustomValidatorConfig();
+    }
+
+}
